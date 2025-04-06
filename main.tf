23c1ae4b7ec14298816e8649f3f837959a8587b6
Santiago <santiago.garcia.2007.22@gmail.com>
2025-04-06 16:53:17 -0500

    Add config files

--- main.tf
+++ main.tf
@@ -34,3 +34,12 @@ resource "azurerm_linux_web_app" "webapp" {
     }
   }
 }
+
+#  Deploy code from a public GitHub repo
+resource "azurerm_app_service_source_control" "sourcecontrol" {
+  app_id             = azurerm_linux_web_app.webapp.id
+  repo_url           = "https://github.com/Santiago20022/Azure-terraform-gaia"
+  branch             = "main"
+  use_manual_integration = true
+  use_mercurial      = false
+}
