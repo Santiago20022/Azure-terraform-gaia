b63325af38147301ca0c53018db27d10798bd760
Santiago <santiago.garcia.2007.22@gmail.com>
2025-04-06 16:42:19 -0500

    Terraform deploy

--- main.tf
+++ main.tf
@@ -4,7 +4,33 @@ provider "azurerm" {
   subscription_id = "66dee015-ef6c-4cbd-a179-55964159584f"
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "terraform-rg"
-  location = "East US"
+# Create the resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "my-example-rg"
+  location = "eastus"
+}
+
+# Create the Linux App Service Plan
+resource "azurerm_service_plan" "appserviceplan" {
+  name                = "my-example-serviceplan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+# Create the web app, pass in the App Service Plan ID
+resource "azurerm_linux_web_app" "webapp" {
+  name                  = "my-example-webapp"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  service_plan_id       = azurerm_service_plan.appserviceplan.id
+  depends_on            = [azurerm_service_plan.appserviceplan]
+  https_only            = true
+  site_config { 
+    minimum_tls_version = "1.2"
+    application_stack {
+      node_version = "22-lts"
+    }
+  }
 }
